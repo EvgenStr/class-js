13d6284a055447d0c67c8086387a753141961201
EvgenStr <juve2910@gmail.com>
2021-04-08 18:48:41 +0300

    add getter and setter for from

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  set from(number) {
+    if (typeof number !== 'number') throw new TypeError();
+    this.from = number;
+  }
+  get from() {
+    return this.from;
+  }
+}
